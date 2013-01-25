b71ee0fcd1a76683fd54b87921b33b838c4cbf75
aimsam <aimsam@macbook.local>
2013-01-25 14:00:09 +0800

    update

--- vd/urls.py
+++ vd/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from api.views import *
-import view
+import views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^hello/(.*)', getIncrementById),
     url(r'^love/(.*)', love),
     url(r'^get_list/$', get_list),
-    url(r'^aaa/$', view.index),
+    url(r'^aaa/$', views.index),
     url(r'^delete/$', forceRefresh)
 #    url(r'^/api/get_video/', hello),
 #    url(r'^/api//', hello),
