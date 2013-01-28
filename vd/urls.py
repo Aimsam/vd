7222df32fc80639535dbfaaa39129d3c0ef55bda
CxC <cxc@WTF.(none)>
2013-01-28 17:18:24 +0800

    add static url support

--- vd/urls.py
+++ vd/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from api.views import *
-import views
+import views,os
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-
-
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -14,6 +12,12 @@ urlpatterns = patterns('',
     url(r'^get_list/$', get_list),
     url(r'^aaa/$', views.index),
     url(r'^delete/$', forceRefresh)
+    url(r'^js/(.*)$','django.views.static.serve',
+        {'document_root':os.path.dirname(__file__)+'vd/js_css/'}
+    )
+    url(r'^css/(.*)$','django.views.static.serve',
+        {'document_root':os.path.dirname(__file__)+'vd/js_css/'}
+    )
 #    url(r'^/api/get_video/', hello),
 #    url(r'^/api//', hello),
 #    url(r'^/api/hello/', hello),
