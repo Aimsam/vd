77f5c9e3c83c36373d95c31078b2580c55c682b2
aimsam <aimsam@macbook.local>
2013-01-31 13:54:37 +0800

    update comments

--- vd/js_css/ajaxJs.js
+++ vd/js_css/ajaxJs.js
@@ -14,8 +14,8 @@ function load_videos(){
                         text += "<div class='title'>" + obj.list[i].title + "| love:" + obj.list[i].love + "</div>";
                         text += "<div class='published'>发布时间:" + obj.list[i].published + "</div>";
                         text += "<div class='duration'>视频时长:" + obj.list[i].duration + "</div>";
-                        text += "<div class='thumbnail'>";
-                        text += "<img src=" + obj.list[i].thumbnail + " /></div>";
+                        text += "<div class='thumbnail'> <a href='http://cuit.sinaapp.com/player_ss.swf?VideoIDS=" + obj.list[i].id+"'>";
+                        text += "<img src=" + obj.list[i].thumbnail + " /></a></div>";
                         text += "<div class='description'>" + obj.list[i].description + "</div>";
                         text += "</div>";
                     }
@@ -27,7 +27,7 @@ function load_videos(){
 }
 
 function load_flash(id) {
-    $("#flash").html("正在加载 flash...");
+    $("#flash").html("正在加载。。。");
     $("#flash").html(
         '<p><object width="480" height="400" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" align="middle"><param name="allowfullscreen" value="true"><param name="quality" value="high"><param name="allowscriptaccess" value="always"><embed width="480" height="400" type="application/x-shockwave-flash" ' +
             'src="http://player.opengg.me/player.php/sid/' + id +
@@ -49,21 +49,18 @@ function load_comment(id) {
     });
 }
 
-window.OnScroll = function(){
-    var a = document.documentElement.scrollTop==0? document.body.clientHeight : document.documentElement.clientHeight;
-    var b = document.documentElement.scrollTop==0? document.body.scrollTop : document.documentElement.scrollTop;
-    var c = document.documentElement.scrollTop==0? document.body.scrollHeight : document.documentElement.scrollHeight;
-    if(a+b==c){
-        $.ajax({
-            url : "http://localhost:8000/get_list/?page="+page++,
-            success : load_videos()
-        });
+window.onscroll = function(){
+    var a = document.documentElement.scrollTop == 0 ? document.body.clientHeight : document.documentElement.clientHeight;
+    var b = document.documentElement.scrollTop == 0 ? document.body.scrollTop : document.documentElement.scrollTop;
+    var c = document.documentElement.scrollTop == 0 ? document.body.scrollHeight : document.documentElement.scrollHeight;
+    if(a + b == c){
+        load_videos();
     }
 }
 
 $(document).ready(function(){
     load_videos();
-    $(document).on("click", ".thumbnail", function() {
+    $(document).on("click", ".title", function() {
         var id = $(this).parent().attr("id");
         load_flash(id);
         load_comment(id);
