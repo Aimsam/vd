1ae20533a6404bd24a4d4a882e734a927f6b8097
aimsam <aimsam@macbook.local>
2013-04-01 12:44:23 +0800

    update

---
+++ api/sphinx.py
@@ -0,0 +1,29 @@
+__author__ = 'aimsam'
+# coding=utf-8
+
+
+
+class SphinxClient(object):
+    def __init__(self):
+        pass
+
+    def hello(self):
+        return "hellp"
+
+
+
+
+#print "hello"
+#
+#print settings.SPHINX["port"]
+
+
+#cli = SphinxClient()
+#cli.SetServer(settings.SPHINX)
+#cli.SetLimits(0,10)
+#cli.SetFilterRange("ppp", 1284652800, 99999999999)
+#cli.AddQuery("UMTcxMzI3OTE2", 'video')
+#result = cli.RunQueries()
+#print result[0]
+#print result[0]['matches']
+#print len(result[0]['matches'])
